b50c4c57565e770eaf5594788df34ba5839114b0
unknown <shady.samara@gmail.com>
2022-06-26 02:29:57 -0700

    instegram2

--- lib/instegram/instegram_feed.dart
+++ lib/instegram/instegram_feed.dart
@@ -11,16 +11,16 @@ class InstegramFeed extends StatelessWidget {
         children: [
           Container(
             margin: EdgeInsets.all(10),
-            child: Row(children:  [
-                          Container(
-                height: 80,
-                width: 80,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage('assets/images/flower.jpg'))),
-              ),
+            child: Row(children: const [
+              //             Container(
+              //   height: 80,
+              //   width: 80,
+              //   decoration: BoxDecoration(
+              //       shape: BoxShape.circle,
+              //       image: DecorationImage(
+              //           fit: BoxFit.cover,
+              //           image: AssetImage('assets/images/flower.jpg'))),
+              // ),
               CircleAvatar(
                 radius: 30,
                 
@@ -30,9 +30,34 @@ class InstegramFeed extends StatelessWidget {
               Text('User',
               style: TextStyle(
                 fontSize: 18,
-                fontWeight: FontWeight.bold),)
+                fontWeight: FontWeight.bold),),
+               Spacer(),
+               Icon(Icons.more_horiz_rounded)
             ]),
           )
+        ,Container(
+          color: Colors.red,
+          height: 300,
+        width: 10000,
+        child: Image.asset('assets/images/flower.jpg',
+        fit: BoxFit.cover,
+        
+        ),)
+       , Container(child: 
+       Container(
+         margin: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+         child: Row(children: [
+           Image.asset('assets/icons/like.png'),
+           const SizedBox(width: 5,),
+           Image.asset('assets/icons/comment.png'),
+            const SizedBox(
+                  width: 5,
+                ),
+           Image.asset('assets/icons/share.png'),
+           const Spacer(),
+           Image.asset('assets/icons/save.png'),
+         ],),
+       ))
         ],
       ),);
   }
